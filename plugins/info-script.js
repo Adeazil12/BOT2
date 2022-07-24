cec8814d98dfb129d284632d4b6dc4ab95610277
Adeazil12 <107722476+Adeazil12@users.noreply.github.com>
2022-07-24 16:09:56 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -8,9 +8,9 @@ let str = `*https://chat.whatsapp.com/H984ganTo0n6IuBlpMMEbh*`
 let wibu = `https://api.zacros.my.id/randomimg/waifu` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/ademods",
-    mediaType: "VIDEO",
-    description: "https://www.instagram.com/ademods2", 
+    mediaUrl: "08980330708",
+    mediaType: "Beli Dong Jangan Ambil Aja",
+    description: "Beli Murah 5k 08980330708", 
     title: 'Ademods MultiDevice',
     body: wm,
     thumbnail: thumb,
